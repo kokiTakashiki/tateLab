a26500f599c3454236316cc3ac15e4ec5d6370c3
koki T <bluewhitered123@gmail.com>
2021-04-05 23:36:37 +0900

    textKit test

---
+++ TextKitTest/TextKitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextKitTest
+//
+//  Created by 武田孝騎 on 2021/04/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
