7b9edbc93f27cdbce1a895b33a9e6a00a7492b05
koki T <bluewhitered123@gmail.com>
2021-04-22 20:25:49 +0900

    Editor

--- TextKitTest/TextKitTest/NoteEditor/ViewController/NoteEditorViewController.swift
+++ TextKitTest/TextKitTest/NoteEditor/ViewController/NoteEditorViewController.swift
@@ -17,5 +17,46 @@ class NoteEditorViewController: UIViewController {
         textView.isScrollEnabled = true
         //textView.text = note.contents
         navigationController?.navigationBar.barStyle = .black
+        notificationCenterAdd()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+      return .lightContent
+    }
+}
+
+// MARK: - UITextViewDelegate
+extension NoteEditorViewController: UITextViewDelegate {
+  func textViewDidEndEditing(_ textView: UITextView) {
+    note.contents = textView.text
+  }
+}
+
+// MARK: - private
+extension NoteEditorViewController {
+    private func notificationCenterAdd() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardDidShow),
+                                               name: UIResponder.keyboardDidShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardDidHide),
+                                               name: UIResponder.keyboardDidHideNotification,
+                                               object: nil)
+    }
+    
+    private func updateTextViewSizeForKeyboardHeight(keyboardHeight: CGFloat) {
+      textView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height - keyboardHeight)
+    }
+    
+    @objc func keyboardDidShow(notification: NSNotification) {
+      if let rectValue = notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue {
+        let keyboardSize = rectValue.cgRectValue.size
+        updateTextViewSizeForKeyboardHeight(keyboardHeight: keyboardSize.height)
+      }
+    }
+    
+    @objc func keyboardDidHide(notification: NSNotification) {
+      updateTextViewSizeForKeyboardHeight(keyboardHeight: 0)
     }
 }
