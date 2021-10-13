eeefb5fc93086a2a2843884885ac6bdcc1b27a3d
koki <bluewhitered123@gmail.com>
2021-10-13 17:58:39 +0900

    add 縦書きプレイグラウンド

---
+++ 縦書きプレイグラウンド.playgroundbook/Edits/UserEdits.diffpack/UserModules/UntitledModule.playgroundmodule/Sources/MongolTextView.swift
@@ -0,0 +1,357 @@
+import UIKit
+
+class UITextViewWithoutMenu: UITextView {
+    
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        
+        return true
+    }
+}
+
+
+public class UIMongolTextView: UIView {
+    
+    
+    // FIXME: long load time
+    
+    // MARK:- Unique to TextView
+    
+    // ********* Unique to TextView *********
+    fileprivate var view = UITextViewWithoutMenu()
+    fileprivate let rotationView = UIView()
+    fileprivate var userInteractionEnabledForSubviews = true
+    fileprivate let mongolFontName = "ChimeeWhiteMirrored"
+    fileprivate let defaultFontSize: CGFloat = 17
+    fileprivate let mongolTextStorage = MongolTextStorage()
+    
+    var text: String {
+        get {
+            //return view.text
+            return mongolTextStorage.unicode
+        }
+        set {
+            mongolTextStorage.unicode = newValue
+            view.text = mongolTextStorage.render()
+            //view.text = newValue
+        }
+    }
+    
+    var fontSize: CGFloat {
+        get {
+            if let font = view.font {
+                return font.pointSize
+            } else {
+                return 0.0
+            }
+        }
+        set {
+            view.font = UIFont(name: mongolFontName, size: newValue)
+        }
+    }
+    
+    var textColor: UIColor {
+        get {
+            if let color = view.textColor {
+                return color
+            } else {
+                return UIColor.black
+            }
+        }
+        set {
+            view.textColor = newValue
+        }
+    }
+    
+    var centerText: Bool {
+        get {
+            return view.textAlignment == NSTextAlignment.center
+        }
+        set {
+            if newValue {
+                view.textAlignment = NSTextAlignment.center
+            }
+        }
+    }
+    
+    var editable: Bool {
+        get {
+            return view.isEditable
+        }
+        set {
+            view.isEditable = newValue
+        }
+    }
+    
+    var selectable: Bool {
+        get {
+            return view.isSelectable
+        }
+        set {
+            view.isSelectable = newValue
+        }
+    }
+    
+    var scrollEnabled: Bool {
+        get {
+            return view.isScrollEnabled
+        }
+        set {
+            view.isScrollEnabled = newValue
+        }
+    }
+    
+    var attributedText: NSAttributedString! {
+        get {
+            return view.attributedText
+        }
+        set {
+            view.attributedText = newValue
+        }
+    }
+    
+    var contentSize: CGSize {
+        get {
+            return CGSize(width: view.contentSize.height, height: view.contentSize.width)
+        }
+        set {
+            view.contentSize = CGSize(width: newValue.height, height: newValue.width)
+        }
+    }
+    
+    
+    
+    var underlyingTextView: UITextViewWithoutMenu {
+        get {
+            return view
+        }
+        set {
+            view = newValue
+        }
+    }
+    
+    // TODO: Switch the sides to account for rotation
+    var textContainerInset: UIEdgeInsets {
+        get {
+            return view.textContainerInset
+        }
+        set {
+            view.textContainerInset = newValue
+        }
+    }
+    
+    public override var intrinsicContentSize : CGSize {
+        return CGSize(width: view.frame.height, height: view.frame.width)
+    }
+    
+    public override func sizeThatFits(_ size: CGSize) -> CGSize {
+        // swap the length and width coming in and going out
+        let fitSize = view.sizeThatFits(CGSize(width: size.height, height: size.width))
+        return CGSize(width: fitSize.height, height: fitSize.width)
+    }
+    
+    func selectedText() -> String? {
+        
+        // TODO: this could give error if selected range has emoji
+        
+        // get the current selected range / cursor position
+        guard let selection = view.selectedTextRange else {
+            return nil
+        }
+        
+        // get caret or selected range
+        // "view.selectedRange" may be easier but use method below as a first step for future emoji support
+        let startGlyphIndex = view.offset(from: view.beginningOfDocument, to: selection.start)
+        let length = view.offset(from: selection.start, to: selection.end)
+        let selectedRange = NSRange(location: startGlyphIndex, length: length)
+        
+        // insert or replace selection with unicode
+        return mongolTextStorage.unicodeForGlyphRange(selectedRange)
+    }
+    
+    func insertMongolText(_ unicode: String) {
+        
+        // get the current selected range / cursor position
+        // TODO: this could give error if selected range has emoji
+        if let selection = view.selectedTextRange {
+            
+            // get caret or selected range
+            // "view.selectedRange" may be easier but use method below as a first step for future emoji support
+            let startGlyphIndex = view.offset(from: view.beginningOfDocument, to: selection.start)
+            let length = view.offset(from: selection.start, to: selection.end)
+            let selectedRange = NSRange(location: startGlyphIndex, length: length)
+            
+            // insert or replace selection with unicode
+            mongolTextStorage.insertUnicodeForGlyphRange(selectedRange, unicodeToInsert: unicode)
+            
+            // render unicode again
+            // FIXME: It is inefficient and unnesessary to render everything
+            view.text = mongolTextStorage.render()
+            
+            // set caret position
+            if let newPosition = view.position(from: view.beginningOfDocument, in: UITextLayoutDirection.right, offset: mongolTextStorage.glyphIndexForCursor) {
+                
+                view.selectedTextRange = view.textRange(from: newPosition, to: newPosition)
+            }
+        }
+        
+    }
+    
+    func replaceWordAtCursorWith(_ replacementString: String) {
+        // get the cursor position
+        if let cursorRange = view.selectedTextRange {
+            let cursorPosition = view.offset(from: view.beginningOfDocument, to: cursorRange.start)
+            mongolTextStorage.replaceWordAtCursorWith(replacementString, atGlyphIndex: cursorPosition)
+            // render unicode again
+            // FIXME: It is inefficient and unnesessary to render everything
+            view.text = mongolTextStorage.render()
+            
+            // set caret position
+            if let newPosition = view.position(from: view.beginningOfDocument, in: UITextLayoutDirection.right, offset: mongolTextStorage.glyphIndexForCursor) {
+                
+                view.selectedTextRange = view.textRange(from: newPosition, to: newPosition)
+            }
+        }
+    }
+    
+    func deleteBackward() {
+        
+        // get the current selected range / cursor position
+        // TODO: this could give error if selected range has emoji
+        if let selection = view.selectedTextRange {
+            
+            // get caret or selected range
+            // "view.selectedRange" may be easier but use method below as a first step for future emoji support
+            let startGlyphIndex = view.offset(from: view.beginningOfDocument, to: selection.start)
+            let length = view.offset(from: selection.start, to: selection.end)
+            let selectedRange = NSRange(location: startGlyphIndex, length: length)
+            
+            // delete unicode backward
+            mongolTextStorage.deleteBackwardsAtGlyphRange(selectedRange)
+            
+            
+            // render unicode again
+            // FIXME: It is inefficient and unnesessary to render everything
+            view.text = mongolTextStorage.render()
+            
+            // set caret position
+            if let newPosition = view.position(from: view.beginningOfDocument, in: UITextLayoutDirection.right, offset: mongolTextStorage.glyphIndexForCursor) {
+                
+                view.selectedTextRange = view.textRange(from: newPosition, to: newPosition)
+            }
+        }
+    }
+    
+    func unicodeCharBeforeCursor() -> String? {
+        
+        // get the cursor position
+        if let cursorRange = view.selectedTextRange {
+            
+            let cursorPosition = view.offset(from: view.beginningOfDocument, to: cursorRange.start)
+            return mongolTextStorage.unicodeCharBeforeCursor(cursorPosition)
+        }
+        return nil
+    }
+    
+    func oneMongolWordBeforeCursor() -> String? {
+        // get the cursor position
+        if let cursorRange = view.selectedTextRange {
+            let cursorPosition = view.offset(from: view.beginningOfDocument, to: cursorRange.start)
+            return mongolTextStorage.unicodeOneWordBeforeCursor(cursorPosition)
+        }
+        return nil
+    }
+    
+    func twoMongolWordsBeforeCursor() -> (String?, String?) {
+        // get the cursor position
+        if let cursorRange = view.selectedTextRange {
+            
+            let cursorPosition = view.offset(from: view.beginningOfDocument, to: cursorRange.start)
+            return mongolTextStorage.unicodeTwoWordsBeforeCursor(cursorPosition)
+        }
+        return (nil, nil)
+    }
+    
+    
+    
+    func setup() {
+        // 1-10: ᠨᠢᠭᠡ ᠬᠤᠶᠠᠷ ᠭᠤᠷᠪᠠ ᠳᠦᠷᠪᠡ ᠲᠠᠪᠤ ᠵᠢᠷᠭᠤᠭ᠎ᠠ ᠳᠤᠯᠤᠭ᠎ᠠ ᠨᠠᠢ᠌ᠮᠠ ᠶᠢᠰᠦ ᠠᠷᠪᠠ
+        
+        // FIXME: UI related settings should go in LayoutSubviews
+        //view.backgroundColor = UIColor.yellowColor()
+        rotationView.isUserInteractionEnabled = userInteractionEnabledForSubviews
+        
+        self.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        self.addSubview(rotationView)
+        rotationView.addSubview(view)
+        
+        // add constraints to pin TextView to rotation view edges.
+        let leadingConstraint = NSLayoutConstraint(item: self.view, attribute: NSLayoutConstraint.Attribute.leading, relatedBy: NSLayoutConstraint.Relation.equal, toItem: rotationView, attribute: NSLayoutConstraint.Attribute.leading, multiplier: 1.0, constant: 0)
+        let trailingConstraint = NSLayoutConstraint(item: self.view, attribute: NSLayoutConstraint.Attribute.trailing, relatedBy: NSLayoutConstraint.Relation.equal, toItem: rotationView, attribute: NSLayoutConstraint.Attribute.trailing, multiplier: 1.0, constant: 0)
+        let topConstraint = NSLayoutConstraint(item: self.view, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: rotationView, attribute: NSLayoutConstraint.Attribute.top, multiplier: 1.0, constant: 0)
+        let bottomConstraint = NSLayoutConstraint(item: self.view, attribute: NSLayoutConstraint.Attribute.bottom, relatedBy: NSLayoutConstraint.Relation.equal, toItem: rotationView, attribute: NSLayoutConstraint.Attribute.bottom, multiplier: 1.0, constant: 0)
+        rotationView.addConstraints([leadingConstraint, trailingConstraint, topConstraint, bottomConstraint])
+        
+        // set font if user didn't specify size in IB
+        if self.view.font?.fontName != mongolFontName {
+            
+            view.font = UIFont(name: mongolFontName, size: defaultFontSize)
+        }
+        
+    }
+    
+    
+    // MARK:- General code for Mongol views
+    
+    // *******************************************
+    // ****** General code for Mongol views ******
+    // *******************************************
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        self.setup()
+    }
+    
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        self.setup()
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        rotationView.transform = CGAffineTransform.identity
+        rotationView.frame = CGRect(origin: CGPoint.zero, size: CGSize(width: self.bounds.height, height: self.bounds.width))
+        rotationView.transform = translateRotateFlip()
+        
+        if self.view.text.isEmpty == false {
+            self.view.scrollRangeToVisible(NSMakeRange(0, 1))
+        }
+        
+        // could do this instead of using constraints
+        //view.frame = rotationView.bounds
+        
+    }
+    
+    func translateRotateFlip() -> CGAffineTransform {
+        
+        var transform = CGAffineTransform.identity
+        
+        // translate to new center
+        transform = transform.translatedBy(x: (self.bounds.width / 2)-(self.bounds.height / 2), y: (self.bounds.height / 2)-(self.bounds.width / 2))
+        // rotate counterclockwise around center
+        transform = transform.rotated(by: CGFloat(-M_PI_2))
+        // flip vertically
+        transform = transform.scaledBy(x: -1, y: -1)
+        
+        return transform
+    }
+    
+    
+}
+
---
+++ 縦書きプレイグラウンド.playgroundbook/Edits/UserEdits.diffpack/UserModules/UntitledModule.playgroundmodule/Sources/ScalarString.swift
@@ -0,0 +1,341 @@
+
+struct ScalarString: Sequence, Hashable, CustomStringConvertible {
+    
+    fileprivate var scalarArray: [UInt32] = []
+    
+    
+    init() {
+        // does anything need to go here?
+    }
+    
+    init(_ character: UInt32) {
+        self.scalarArray.append(character)
+    }
+    
+    init(_ charArray: [UInt32]) {
+        for c in charArray {
+            self.scalarArray.append(c)
+        }
+    }
+    
+    init(_ string: String) {
+        
+        for s in string.unicodeScalars {
+            self.scalarArray.append(s.value)
+        }
+    }
+    
+    // Generator in order to conform to SequenceType protocol
+    // (to allow users to iterate as in `for myScalarValue in myScalarString` { ... })
+    func makeIterator() -> AnyIterator<UInt32> {
+        return AnyIterator(scalarArray.makeIterator())
+    }
+    
+    // append
+    mutating func append(_ scalar: UInt32) {
+        self.scalarArray.append(scalar)
+    }
+    
+    mutating func append(_ scalarString: ScalarString) {
+        for scalar in scalarString {
+            self.scalarArray.append(scalar)
+        }
+    }
+    
+    mutating func append(_ string: String) {
+        for s in string.unicodeScalars {
+            self.scalarArray.append(s.value)
+        }
+    }
+    
+    // charAt
+    func charAt(_ index: Int) -> UInt32 {
+        return self.scalarArray[index]
+    }
+    
+    // clear
+    mutating func clear() {
+        self.scalarArray.removeAll(keepingCapacity: true)
+    }
+    
+    // contains
+    func contains(_ character: UInt32) -> Bool {
+        for scalar in self.scalarArray {
+            if scalar == character {
+                return true
+            }
+        }
+        return false
+    }
+    
+    // description (to implement Printable protocol)
+    var description: String {
+        return self.toString()
+    }
+    
+    // endsWith
+    func endsWith() -> UInt32? {
+        return self.scalarArray.last
+    }
+    
+    // indexOf
+    // returns first index of scalar string match
+    func indexOf(_ string: ScalarString) -> Int? {
+        
+        if scalarArray.count < string.length {
+            return nil
+        }
+        
+        for i in 0...(scalarArray.count - string.length) {
+            
+            for j in 0..<string.length {
+                
+                if string.charAt(j) != scalarArray[i + j] {
+                    break // substring mismatch
+                }
+                if j == string.length - 1 {
+                    return i
+                }
+            }
+        }
+        
+        return nil
+    }
+    
+    // insert
+    mutating func insert(_ scalar: UInt32, atIndex index: Int) {
+        self.scalarArray.insert(scalar, at: index)
+    }
+    mutating func insert(_ string: ScalarString, atIndex index: Int) {
+        var newIndex = index
+        for scalar in string {
+            self.scalarArray.insert(scalar, at: newIndex)
+            newIndex += 1
+        }
+    }
+    mutating func insert(_ string: String, atIndex index: Int) {
+        var newIndex = index
+        for scalar in string.unicodeScalars {
+            self.scalarArray.insert(scalar.value, at: newIndex)
+            newIndex += 1
+        }
+    }
+    
+    // isEmpty
+    var isEmpty: Bool {
+        return self.scalarArray.count == 0
+    }
+    
+    // hashValue (to implement Hashable protocol)
+    var hashValue: Int {
+        
+        // DJB Hash Function
+        return self.scalarArray.reduce(5381) {
+            ($0 << 5) &+ $0 &+ Int($1)
+        }
+    }
+    
+    // length
+    var length: Int {
+        return self.scalarArray.count
+    }
+    
+    // remove character
+    mutating func removeCharAt(_ index: Int) {
+        self.scalarArray.remove(at: index)
+    }
+    func removingAllInstancesOfChar(_ character: UInt32) -> ScalarString {
+        
+        var returnString = ScalarString()
+        
+        for scalar in self.scalarArray {
+            if scalar != character {
+                returnString.append(scalar)
+            }
+        }
+        
+        return returnString
+    }
+    func removeRange(_ range: CountableRange<Int>) -> ScalarString? {
+        
+        if range.lowerBound < 0 || range.upperBound > scalarArray.count {
+            return nil
+        }
+        
+        var returnString = ScalarString()
+        
+        for i in 0..<scalarArray.count {
+            if i < range.lowerBound || i >= range.upperBound {
+                returnString.append(scalarArray[i])
+            }
+        }
+        
+        return returnString
+    }
+    
+    
+    // replace
+    func replace(_ character: UInt32, withChar replacementChar: UInt32) -> ScalarString {
+        
+        var returnString = ScalarString()
+        
+        for scalar in self.scalarArray {
+            if scalar == character {
+                returnString.append(replacementChar)
+            } else {
+                returnString.append(scalar)
+            }
+        }
+        return returnString
+    }
+    func replace(_ character: UInt32, withString replacementString: String) -> ScalarString {
+        
+        var returnString = ScalarString()
+        
+        for scalar in self.scalarArray {
+            if scalar == character {
+                returnString.append(replacementString)
+            } else {
+                returnString.append(scalar)
+            }
+        }
+        return returnString
+    }
+    func replaceRange(_ range: CountableRange<Int>, withString replacementString: ScalarString) -> ScalarString {
+        
+        var returnString = ScalarString()
+        
+        for i in 0..<scalarArray.count {
+            if i < range.lowerBound || i >= range.upperBound {
+                returnString.append(scalarArray[i])
+            } else if i == range.lowerBound {
+                returnString.append(replacementString)
+            }
+        }
+        return returnString
+    }
+    
+    // set (an alternative to myScalarString = "some string")
+    mutating func set(_ string: String) {
+        self.scalarArray.removeAll(keepingCapacity: false)
+        for s in string.unicodeScalars {
+            self.scalarArray.append(s.value)
+        }
+    }
+    
+    // split
+    func split(atChar splitChar: UInt32) -> [ScalarString] {
+        var partsArray: [ScalarString] = []
+        if self.scalarArray.count == 0 {
+            return partsArray
+        }
+        var part: ScalarString = ScalarString()
+        for scalar in self.scalarArray {
+            if scalar == splitChar {
+                partsArray.append(part)
+                part = ScalarString()
+            } else {
+                part.append(scalar)
+            }
+        }
+        partsArray.append(part)
+        return partsArray
+    }
+    
+    // startsWith
+    func startsWith() -> UInt32? {
+        return self.scalarArray.first
+    }
+    
+    // substring
+    func substring(_ startIndex: Int) -> ScalarString {
+        // from startIndex to end of string
+        var subArray: ScalarString = ScalarString()
+        for i in startIndex..<self.length {
+            subArray.append(self.scalarArray[i])
+        }
+        return subArray
+    }
+    func substring(_ startIndex: Int, _ endIndex: Int) -> ScalarString {
+        // (startIndex is inclusive, endIndex is exclusive)
+        var subArray: ScalarString = ScalarString()
+        for i in startIndex..<endIndex {
+            subArray.append(self.scalarArray[i])
+        }
+        return subArray
+    }
+    
+    // toString
+    func toString() -> String {
+        var string: String = ""
+        
+        for scalar in self.scalarArray {
+            if let validScalor = UnicodeScalar(scalar) {
+                string.append(Character(validScalor))
+            }
+        }
+        return string
+    }
+    
+    // trim
+    // removes leading and trailing whitespace (space, tab, newline)
+    func trim() -> ScalarString {
+        
+        //var returnString = ScalarString()
+        let space: UInt32 = 0x00000020
+        let tab: UInt32 = 0x00000009
+        let newline: UInt32 = 0x0000000A
+        
+        var startIndex = self.scalarArray.count
+        var endIndex = 0
+        
+        // leading whitespace
+        for i in 0..<self.scalarArray.count {
+            if self.scalarArray[i] != space &&
+                self.scalarArray[i] != tab &&
+                self.scalarArray[i] != newline {
+                
+                startIndex = i
+                break
+            }
+        }
+        
+        // trailing whitespace
+        for i in stride(from: (self.scalarArray.count - 1), through: 0, by: -1) {
+            if self.scalarArray[i] != space &&
+                self.scalarArray[i] != tab &&
+                self.scalarArray[i] != newline {
+                
+                endIndex = i + 1
+                break
+            }
+        }
+        
+        if endIndex <= startIndex {
+            return ScalarString()
+        }
+        
+        return self.substring(startIndex, endIndex)
+    }
+    
+    // values
+    func values() -> [UInt32] {
+        return self.scalarArray
+    }
+    
+}
+
+func ==(left: ScalarString, right: ScalarString) -> Bool {
+    return left.scalarArray == right.scalarArray
+}
+
+func +(left: ScalarString, right: ScalarString) -> ScalarString {
+    var returnString = ScalarString()
+    for scalar in left.values() {
+        returnString.append(scalar)
+    }
+    for scalar in right.values() {
+        returnString.append(scalar)
+    }
+    return returnString
+}
