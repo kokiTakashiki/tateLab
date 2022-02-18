99110cbcbc5db36254a09dbd7755f6380d84a368
kokiTakeda <bluewhitered123@gmail.com>
2022-02-19 00:47:02 +0900

    view

---
+++ GenkouyoushiView01/GenkouyoushiView01/modules/TateEditorView/Views/TateEditorViewController.swift
@@ -0,0 +1,56 @@
+//
+//  TateEditorViewController.swift
+//  GenkouyoushiView01
+//
+//  Created by 武田孝騎 on 2022/02/19.
+//
+
+import UIKit
+
+@MainActor
+protocol TateEditorUserInterface: AnyObject {
+}
+
+@MainActor
+final class TateEditorViewController: UIViewController {
+
+    // MARK: Type Aliases
+
+    // MARK: Stored Instance Properties
+    
+    private var presenter: TateEditorEventHandler!
+    
+    //private var editData: TateEditEntity!
+
+    // MARK: Computed Instance Properties
+
+    // MARK: IBOutlets
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //editData = TateEditEntity(title: "test", description: "2022/01/29", content: textView.text)
+        
+        Task {
+            await presenter.viewDidLoad()
+        }
+    }
+    
+    // MARK: IBActions
+
+    // MARK: Other Internal Methods
+
+    func inject(
+        presenter: TateEditorEventHandler
+        //logger: LoggerProtocol = Logger.default
+    ) {
+        self.presenter = presenter
+        //self.logger = logger
+    }
+
+    // MARK: Other Private Methods
+}
+
+extension TateEditorViewController: TateEditorUserInterface {
+}
